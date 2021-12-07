a153b1c94d9d4bd09edb128f7795d054fee0df70
bit doer <bitdoer@gmail.com>
2021-12-07 02:33:23 -0600

    add day 7 part 2

--- day7-2021/src/main.rs
+++ day7-2021/src/main.rs
@@ -1,4 +1,25 @@
-use std::fs;
+use std::{cmp::min, fs};
+
+fn part1_deviation(parsed: &[i64], center: i64) -> i64 {
+    let mut output = 0;
+    for &entry in parsed {
+        output += (entry - center).abs()
+    }
+    output
+}
+
+fn part2_deviation(parsed: &[i64], center: i64) -> i64 {
+    let mut output = 0;
+    for &entry in parsed {
+        let n = (entry - center).abs();
+        // the deviations described in the problem are exactly triangular
+        // numbers---a distance of 3 gives 1 + 2 + 3 fuel, for example---
+        // and the below formula gives the general amount of fuel for a
+        // distance n
+        output += (n * n + n) / 2;
+    }
+    output
+}
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Failed to read file");
@@ -10,18 +31,30 @@ fn main() {
     parsed.sort_unstable();
     // the median is exactly the number which has minimal average deviation from the
     // elements of a data set, and therefore minimal total deviation
-    //
-    // if the median is not a whole number, then the proper candidates are exactly
-    // 
     let median = if parsed.len() % 2 == 0 {
-        (parsed[(parsed.len() / 2) - 1] + parsed[parsed.len() / 2]) / 2
+        (parsed[(parsed.len() / 2) - 1] + parsed[parsed.len() / 2]) as f64 / 2.0
     } else {
-        parsed[parsed.len() / 2]
+        parsed[parsed.len() / 2] as f64
     };
-    let mut part1 = 0;
-    for &entry in &parsed {
-        part1 += (entry - median).abs();
-    }
+    // if the median is not a whole number, then the proper candidates to answer
+    // this problem are the integers directly above and below it
+    let part1 = if median.floor() == median {
+        part1_deviation(&parsed, median as i64)
+    } else {
+        min(
+            part1_deviation(&parsed, median.floor() as i64),
+            part1_deviation(&parsed, median.ceil() as i64),
+        )
+    };
     println!("{}", part1);
-    // part 2 is more subtle
+    // part 2, dealing with a quadratic variation, is minimized instead by the mean;
+    // since the mean is not available as such (being non-integral), we take a similar
+    // tack, considering exactly those two integers immediately surrounding it as
+    // candidates
+    let mean = parsed.iter().sum::<i64>() as f64 / parsed.len() as f64;
+    let part2 = min(
+        part2_deviation(&parsed, mean.floor() as i64),
+        part2_deviation(&parsed, mean.ceil() as i64),
+    );
+    println!("{}", part2);
 }
